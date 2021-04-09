42a7ade5daaa0c6f7859f98c80fab1cfeead3174
Ngireddy <somulanagireddy@gmail.com>
2021-04-09 19:16:42 +0530

    [Nagireddy]: added Parameterised test caese

---
+++ src/test/java/Bridgelabzs/EmailListTest.java
@@ -0,0 +1,58 @@
+package Bridgelabzs;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class EmailListTest {
+
+    private String argument;
+    private boolean expectedValidation;
+
+    public EmailListTest(String argument, boolean expectedValidation) {
+        this.argument = argument;
+        this.expectedValidation = expectedValidation;
+    }
+
+    @Parameterized.Parameters
+    public static List<Object[]> data() {
+        Object[][] data1 = new Object[][]{
+                {"abc@yahoo.com", true},
+                {"abc-100@yahoo.com", true},
+                {"abc.100@yahoo.com", true},
+                {"bc111@abc.com", true},
+                {"abc-100@abc.net", true},
+                {"abc.100@abc.com.au", true},
+                {"abc@1.com", true},
+                {"abc@gmail.com.com", true},
+                {"abc+100@gmail.com", true},
+                {"a.b1.00@abc.net", true},
+                {"abc", false},
+                {"abc@.com.my", false},
+                {"abc123@gmail.a", false},
+                {"abc123@.com", false},
+                {"abc123@.com.com", false},
+                {".abc@abc.com", false},
+                {"abc()*@gmail.com", false},
+                {"abc@%*.com", false},
+                {"abc..2002@gmail.com", false},
+                {"abc.@gmail.com", false},
+                {"abc@abc@gmail.com", false},
+                {"abc@gmail.com.1a", false},
+                {"abc@gmail.com.aa.au", false}
+        };
+        return Arrays.asList(data1);
+    }
+
+    @Test
+    public void testEmailList() {
+        boolean result = new RegexUserRegistration().validatePattern(argument, RegexUserRegistration.emailRegExpPattern);
+        Assertions.assertEquals(result, expectedValidation);
+    }
+}
--- src/test/java/Bridgelabzs/regexUserRegistrationTest.java
+++ src/test/java/Bridgelabzs/regexUserRegistrationTest.java
@@ -7,51 +7,52 @@ public class regexUserRegistrationTest {
     @Test
     public void givenFirstName_WhenValid_ShouldReturnTrue(){
         String firstName = "Shubham";
-        Assertions.assertTrue(regexUserRegistration.validatePattern(firstName,regexUserRegistration.firstNameRegExpPattern));
+        Assertions.assertTrue(RegexUserRegistration.validatePattern(firstName, RegexUserRegistration.firstNameRegExpPattern));
     }
     @Test
     public void givenFirstName_WhenInvalid_ShouldReturnFalse(){
         String firstName = "Sh";
-        Assertions.assertFalse(regexUserRegistration.validatePattern(firstName,regexUserRegistration.firstNameRegExpPattern));
+        Assertions.assertFalse(RegexUserRegistration.validatePattern(firstName, RegexUserRegistration.firstNameRegExpPattern));
     }
     @Test
     public void givenLastName_WhenValid_ShouldReturnTrue(){
         String firstName = "Phoujdar";
-        Assertions.assertTrue(regexUserRegistration.validatePattern(firstName,regexUserRegistration.firstNameRegExpPattern));
+        Assertions.assertTrue(RegexUserRegistration.validatePattern(firstName, RegexUserRegistration.lastNameRegExpPattern));
     }
     @Test
     public void givenLastName_WhenInvalid_ShouldReturnFalse(){
         String firstName = "ph";
-        Assertions.assertFalse(regexUserRegistration.validatePattern(firstName,regexUserRegistration.firstNameRegExpPattern));
+        Assertions.assertFalse(RegexUserRegistration.validatePattern(firstName, RegexUserRegistration.lastNameRegExpPattern));
     }
     @Test
     public void givenEmail_WhenValid_ShouldReturnTrue(){
         String firstName = "a.b1.00@abc.net";
-        Assertions.assertTrue(regexUserRegistration.validatePattern(firstName,regexUserRegistration.emailRegExpPattern));
+        Assertions.assertTrue(RegexUserRegistration.validatePattern(firstName, RegexUserRegistration.emailRegExpPattern));
     }
     @Test
     public void givenEmail_WhenInvalid_ShouldReturnFalse(){
         String firstName = "abc";
-        Assertions.assertFalse(regexUserRegistration.validatePattern(firstName,regexUserRegistration.emailRegExpPattern));
+        Assertions.assertFalse(RegexUserRegistration.validatePattern(firstName, RegexUserRegistration.emailRegExpPattern));
     }
     @Test
     public void givenMobileNumber_WhenValid_ShouldReturnTrue(){
         String firstName = "91 9822056132";
-        Assertions.assertTrue(regexUserRegistration.validatePattern(firstName,regexUserRegistration.mobileNumberRegExpPattern));
+        Assertions.assertTrue(RegexUserRegistration.validatePattern(firstName, RegexUserRegistration.mobileNumberRegExpPattern));
     }
     @Test
     public void givenMobileNumber_WhenInvalid_ShouldReturnFalse(){
         String firstName = "+919822056132";
-        Assertions.assertFalse(regexUserRegistration.validatePattern(firstName,regexUserRegistration.mobileNumberRegExpPattern));
+        Assertions.assertFalse(RegexUserRegistration.validatePattern(firstName, RegexUserRegistration.mobileNumberRegExpPattern));
     }
     @Test
     public void givenPassword_WhenValid_ShouldReturnTrue(){
         String firstName = "asdU2@q4515";
-        Assertions.assertTrue(regexUserRegistration.validatePattern(firstName,regexUserRegistration.passwordRegExpPattern));
+        Assertions.assertTrue(RegexUserRegistration.validatePattern(firstName, RegexUserRegistration.passwordRegExpPattern));
     }
     @Test
     public void givenPassword_WhenInvalid_ShouldReturnFalse(){
         String firstName = "adwssdfrgsadw";
-        Assertions.assertFalse(regexUserRegistration.validatePattern(firstName,regexUserRegistration.passwordRegExpPattern));
+        Assertions.assertFalse(RegexUserRegistration.validatePattern(firstName, RegexUserRegistration.passwordRegExpPattern));
     }
+
 }
